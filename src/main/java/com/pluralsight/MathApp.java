2e2bfcbfad6ff449e536413b32e1536ab6375be6
arsenii <arseniy.kunilovskiy@gmail.com>
2025-09-24 16:02:14 -0400

    Questions 1-8

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -19,5 +19,39 @@ public class MathApp {
         double radius = 7.25;
         System.out.println("Area of a circle is: " + Math.PI*Math.pow(radius, 2));
         System.out.println(); //just a new line to divide questions
+
+        //question four
+        double hypotenuse = 5.0;
+        System.out.println("Square root of hypotenuse is " + Math.sqrt(hypotenuse));
+        System.out.println(); //just a new line to divide questions
+
+        //question five
+        int x1 = 5;
+        int x2 = 85;
+        int y1 = 10;
+        int y2 = 50;
+        double disntace = Math.sqrt(Math.pow((y1 - y2),2) + Math.pow((x1 - x2),2));
+        System.out.println("The distance between two points: " + disntace);
+        System.out.println(); //just a new line to divide questions
+
+        //question six
+        double negativeValue = -3.8;
+        double absValue = Math.abs(negativeValue);
+        System.out.println("The absolute value of " + negativeValue + " is " + absValue);
+        System.out.println(); //just a new line to divide questions
+
+        //question seven
+        System.out.println("Random number between 0 and 1 is " + Math.random());
+        System.out.println(); //just a new line to divide questions
+
+        //question eight
+        int days = 24;
+        int hours = 24;
+        int minutes = 60;
+        int seconds = 60;
+        System.out.println(days*hours*minutes*seconds + " is number of seconds in 24 days");
+
+        System.out.println(); //just a new line to divide questions
+
     }
 }
